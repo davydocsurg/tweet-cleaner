f1d890a114b37fa71cb0dd0a325a2e4ec8e53f25
davydocsurg <davydocsurg@gmail.com>
2023-03-02 10:23:34 +0100

    add method for fetching and deleting tweets from the past number of days specified

--- index.py
+++ index.py
@@ -11,3 +11,15 @@ access_token_secret = 'your_access_token_secret'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+
+# get tweets from the past 14 days
+days = 14
+now = datetime.now()
+week_ago = now - timedelta(days=days)
+tweets = tweepy.Cursor(api.user_timeline).items()
+tweets_to_delete = [tweet for tweet in tweets if tweet.created_at < week_ago]
+
+# delete old tweets
+for tweet in tweets_to_delete:
+    api.destroy_status(tweet.id)
+    print(f'Deleted tweet with ID {tweet.id_str}')
