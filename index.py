c84e80c837926cd7cd1bfa29462d78ac24836ffb
davydocsurg <davydocsurg@gmail.com>
2023-03-02 10:21:50 +0100

    add Twitter API authentication method

--- index.py
+++ index.py
@@ -6,3 +6,8 @@ consumer_key = 'your_consumer_key'
 consumer_secret = 'your_consumer_secret'
 access_token = 'your_access_token'
 access_token_secret = 'your_access_token_secret'
+
+# authenticate with Twitter API
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
